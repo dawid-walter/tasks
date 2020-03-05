d429bc87ef8c4a0dde68ed76896b7cbefd626cb4
davedohc <davedohc@aim.com>
2020-03-05 11:22:46 +0000

    Kodilla 22.2 - lesson

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,30 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/trello")
+public class TrelloController {
+
+    private TrelloClient trelloClient;
+    @Autowired
+    public void setTrelloClient(TrelloClient trelloClient) {
+        this.trelloClient = trelloClient;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
+    public void getTrelloBoards() {
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+
+        trelloBoards.forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+
+    }
+}
